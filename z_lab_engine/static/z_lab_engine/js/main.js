4707b8140c00f39889d76b99e616e00f6493f44b
knlgdsn <hmenguaslan15@ku.edu.tr>
2019-08-01 15:58:53 +0300

    added plus sign and correction of a few lines

--- z_lab_engine/static/z_lab_engine/js/main.js
+++ z_lab_engine/static/z_lab_engine/js/main.js
@@ -2,30 +2,30 @@
 (function ($) {
     "use strict";
 
-
+        var input = $('.validate-input .input100');
     /*==================================================================
     [ Validate after type ]*/
-    $('.validate-input .input100').each(function(){
+    input.each(function(){
         $(this).on('blur', function(){
-            if(validate(this) == false){
+            if(validate(this) === false){
                 showValidate(this);
             }
             else {
                 $(this).parent().addClass('true-validate');
             }
         })    
-    })
+    });
   
   
     /*==================================================================
     [ Validate ]*/
-    var input = $('.validate-input .input100');
+
 
     $('.validate-form').on('submit',function(){
         var check = true;
 
         for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
+            if(validate(input[i]) === false){
                 showValidate(input[i]);
                 check=false;
             }
@@ -35,7 +35,7 @@
     });
 
 
-    $('.validate-form .input100').each(function(){
+    input.each(function(){
         $(this).focus(function(){
            hideValidate(this);
            $(this).parent().removeClass('true-validate');
@@ -43,13 +43,13 @@
     });
 
      function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+        if($(input).attr('type') === 'email' || $(input).attr('name') === 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(]?)$/) == null) {
                 return false;
             }
         }
         else {
-            if($(input).val().trim() == ''){
+            if($(input).val().trim() === ''){
                 return false;
             }
         }
@@ -57,10 +57,9 @@
 
     function showValidate(input) {
         var thisAlert = $(input).parent();
-
         $(thisAlert).addClass('alert-validate');
 
-        $(thisAlert).append('<span class="btn-hide-validate">&#xf136;</span>')
+        $(thisAlert).append('<span class="btn-hide-validate">&#xf136;</span>');
         $('.btn-hide-validate').each(function(){
             $(this).on('click',function(){
                hideValidate(this);

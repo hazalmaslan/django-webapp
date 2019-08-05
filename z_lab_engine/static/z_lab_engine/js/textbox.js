426d58cad9bf7c3f945e7c849f295debb173eb1a
knlgdsn <hmenguaslan15@ku.edu.tr>
2019-08-05 17:38:59 +0300

    all the adding functions are added to javascript and html code is restructured accordingly

--- z_lab_engine/static/z_lab_engine/js/textbox.js
+++ z_lab_engine/static/z_lab_engine/js/textbox.js
@@ -11,20 +11,27 @@ function addSizeVal() {
     var sizeType = "";
     var sizeMag = "";
 
-    if (sizeVal !== "" || sizeType === "" || sizeMag === "") {
+    if (sizeVal !== "" || (typeof sizeType === 'undefined' || typeof sizeMag === 'undefined')) {
         sizeType = $('input[name=size-val]:checked').val();
         sizeMag = $('input[name=size-mag]:checked').val();
     }
-    else{
-        alert("Please select all the choices");
-    }
-    if(typeof sizeVal === 'number'){
 
-    }
     var str = "size:" + sizeVal + " " + sizeType + " " + sizeMag;
     if (document.getElementById("plus-size-button").checked) {
         arr.push(str);
     } else {
+        document.getElementsByName("size-val").forEach(
+            function (element) {
+                element.checked = false;
+            }
+        );
+
+        document.getElementsByName("size-mag").forEach(
+            function (element) {
+                element.checked = false;
+            }
+        );
+        document.getElementById("size").value = "";
         arr = remove(arr, str);
     }
 
@@ -55,10 +62,6 @@ function addTypeVal() {
                     arr.push(typeArr[index]);
                 }
             }
-            console.log("adding");
-            console.log(typeArr);
-            console.log(arr);
-
         }
         else{
             document.getElementsByName("type").forEach(function (element) {
@@ -75,48 +78,73 @@ function addTypeVal() {
             for (index = 0; index < deletedArr.length; index++){
                 arr = remove(arr, deletedArr[index]);
             }
-            console.log("removing");
-            console.log(typeArr);
-            console.log(arr);
         }
     }
 }
 
 function addPositivesVal() {
-    var posVal = document.getElementById("pos").toString();
-    if (posVal !== "") {
-        document.getElementsByName("positives").forEach(function(element) {
-            if (element.isChecked()) {
-                arr.push("positive:" + posVal + " " + element.getAttribute(value));
-            } else {
-                remove(arr, "positive:" + posVal + " " + element.getAttribute(value));
-            }
-        });
+    var posVal = document.getElementById("positives").value;
+    var str = "";
+    var posMag = "";
+    document.getElementsByName("positives").forEach(function(element) {
+        if (element.checked) {
+            posMag = element.value;
+        }
+    });
+    str = "positive:" + posVal + " " + posMag;
+    if(document.getElementById("plus-positives-button").checked){
+        arr.push(str);
     }
+    else{
+        if (arr.includes(str)){
+            console.log(str);
+            arr = remove(arr, str);
+        }
+        document.getElementsByName("positives").forEach(function (element) {
+            element.checked = false;
+        });
+        document.getElementById("positives").value = "";
+    }
+
 }
 
 function addChildrenPositivesVal() {
-    var childrenPosVal = document.getElementById("child-pos").toString();
-
-    if (childrenPosVal !== "") {
-        document.getElementsByName("positives").forEach(function(element) {
-            if (element.isChecked()) {
-                arr.push("children-positive:" + childrenPosVal + " " + element.getAttribute(value));
-            } else {
-                remove(arr, "children-positive:" + childrenPosVal + " " + element.getAttribute(value));
-            }
+    var posVal = document.getElementById("children-positives").value;
+    var str;
+    var posMag = "";
+    document.getElementsByName("children-positives").forEach(function(element) {
+        if (element.checked) {
+            posMag = element.value;
+        }
+    });
+    str = "children-positive:" + posVal + " " + posMag;
+    if(document.getElementById("plus-children-pos-button").checked){
+        arr.push(str);
+    }
+    else{
+        if (arr.includes(str)){
+            console.log(str);
+            arr = remove(arr, str);
+        }
+        document.getElementsByName("children-positives").forEach(function (element) {
+            element.checked = false;
         });
+        document.getElementById("children-positives").value = "";
     }
 }
 
 function addFileName() {
-    var fileName = document.getElementById("file-name").toString();
-    if (fileName !== "") {
-        if ($("#file-name-button:checked")) {
-            arr.push("file name:" + fileName);
-        } else {
-            remove(arr, "file name:" + fileName);
-        }
+    var fileName = document.getElementById("file-name");
+
+    if (fileName.value !== "") {
+        var str = "file name:" + fileName.value;
+    }
+    if(document.getElementById("plus-file-name-button").checked){
+        arr.push(str);
+    }
+    else{
+        arr = remove(arr, str);
+        fileName.value = "";
     }
 }
 
@@ -133,85 +161,161 @@ function addLADate() {
 }
 
 function addSourcesVal() {
-    var sources = document.getElementById("sources").toString();
+    var sources = document.getElementById("sources").value;
+    var str;
+    var sourceMag = "";
+    document.getElementsByName("sources").forEach(function(element) {
+        if (element.checked) {
+            sourceMag = element.value;
+        }
+    });
+    str = "sources:" + sources + " " + sourceMag;
 
-    if (sources !== "") {
-        document.getElementsByName("sources").forEach(function(element) {
-            if (element.isChecked()) {
-                arr.push("sources:" + sources + " " + element.getAttribute(value));
-            } else {
-                remove(arr, "sources:" + sources + " " + element.getAttribute(value));
-            }
-        });
+    if(document.getElementById("plus-sources-button").checked){
+        arr.push(str);
     }
+    else{
+        if (arr.includes(str)){
+            console.log(str);
+            arr = remove(arr, str);
+        }
+        document.getElementsByName("sources").forEach(function (element) {
+            element.checked = false;
+        });
+        document.getElementById("sources").value = "";
+    }
+
 }
 
 function addSubmissionsVal() {
-    var submissions = document.getElementById("submission").toString();
+    var submissions = document.getElementById("submission").value;
+    var str;
+    var subMag = "";
+    document.getElementsByName("submissions").forEach(function(element) {
+        if (element.checked) {
+            subMag = element.value;
+        }
+    });
+    str = "submissions:" + submissions + " " + subMag;
 
-    if (submissions !== "" && document.getElementById("submission-button").isChecked()) {
-        document.getElementsByName("submissions").forEach(function(element) {
-            if (element.isChecked()) {
-                arr.push("submission:" + submissions + " " + element.getAttribute(value));
-            } else {
-                remove(arr, "submission:" + submissions + " " + element.getAttribute(value));
-            }
+    if(document.getElementById("plus-submissions-button").checked){
+        arr.push(str);
+    }
+    else{
+        if (arr.includes(str)){
+            console.log(str);
+            arr = remove(arr, str);
+        }
+        document.getElementsByName("submissions").forEach(function (element) {
+            element.checked = false;
         });
+        document.getElementById("submission").value = "";
     }
 }
 
 function addSubmitterVal() {
-    var submitter = document.getElementById("file-name").toString();
-    if (submitter !== "") {
-        if ($("#submitter-button:checked")) {
-            arr.push("submitter:" + submitter);
-        } else {
-            remove(arr, "submitter:" + submitter);
-        }
+    var submitter = document.getElementById("submitter");
+
+    if (submitter.value !== "") {
+        var str = "submitter:" + submitter.value;
+    }
+    if(document.getElementById("plus-submitter-button").checked){
+        arr.push(str);
+    }
+    else{
+        arr = remove(arr, str);
+        submitter.value = "";
     }
 }
 
 function addTaggedValues() {
+    var taggedArr = [];
+    var deletedArr = [];
     document.getElementsByName("tagged").forEach(function(element) {
-        if (element.isChecked()) {
-            arr.push(element.getAttribute(value));
-        } else if (element.isUnchecked()) {
-            remove(arr, element.getAttribute(value));
-        }
+    if (element.checked) {
+        taggedArr.push(element.value);
+    } else {
+        deletedArr.push(element.value);
+    }
     });
+    if(taggedArr.length === 0){
+        document.getElementById("plus-tagged-button").checked = false;
+        alert("PLEASE SELECT A TYPE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+    else{
+        var index;
+        if(document.getElementById("plus-tagged-button").checked){
+            for (index = 0; index < deletedArr.length; index++){
+                taggedArr = remove(taggedArr, deletedArr[index]);
+            }
+            for (index = 0; index < taggedArr.length; index++){
+                if(!arr.includes(taggedArr[index])){
+                    arr.push(taggedArr[index]);
+                }
+            }
+        }
+        else{
+            document.getElementsByName("tagged").forEach(function (element) {
+                if(element.checked && taggedArr.includes(element.value)){
+                    element.checked = false;
+                    taggedArr = remove(taggedArr, element.value);
+                    deletedArr.push(arr, element.value);
+                }
+            });
+            for (index = 0; index < deletedArr.length; index++){
+                taggedArr = remove(taggedArr, deletedArr[index]);
+            }
+
+            for (index = 0; index < deletedArr.length; index++){
+                arr = remove(arr, deletedArr[index]);
+            }
+        }
+    }
 }
 
 function addSectionVal() {
-    var section = document.getElementById("file-name").toString();
-    if (section !== "") {
-        if ($("#section-button:checked")) {
-            arr.push("section:" + section);
-        } else {
-            remove(arr, "section:" + section);
-        }
+    var section = document.getElementById("section");
+
+    if (section.value !== "") {
+        var str = "section:" + section.value;
+    }
+    if(document.getElementById("plus-section-button").checked){
+        arr.push(str);
+    }
+    else{
+        arr = remove(arr, str);
+        section.value = "";
     }
 
 }
 
 function addImportVal() {
-    var imports = document.getElementById("file-name").toString();
-    if (imports !== "") {
-        if ($("#imports-button:checked")) {
-            arr.push("imports:" + imports);
-        } else {
-            remove(arr, "imports:" + imports);
-        }
+    var imports = document.getElementById("imports");
+
+    if (imports.value !== "") {
+        var str = "imports:" + imports.value;
+    }
+    if(document.getElementById("plus-imports-button").checked){
+        arr.push(str);
+    }
+    else{
+        arr = remove(arr, str);
+        imports.value = "";
     }
 }
 
 function addExportVal() {
-    var exports = document.getElementById("file-name").toString();
-    if (exports !== "") {
-        if ($("#exports-button:checked")) {
-            arr.push("exports:" + exports);
-        } else {
-            remove(arr, "exports:" + exports);
-        }
+    var exports = document.getElementById("exports");
+
+    if (exports.value !== "") {
+        var str = "exports:" + exports.value;
+    }
+    if(document.getElementById("plus-exports-button").checked){
+        arr.push(str);
+    }
+    else{
+        arr = remove(arr, str);
+        exports.value = "";
     }
 }
 
@@ -229,11 +333,6 @@ $(".plus").change(function() {
     }
 
 });
-
-function add() {
-
-}
-
 
 var grd = function() {
     $("input[type='radio']").click(function() {
